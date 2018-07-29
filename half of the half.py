65b8b6e8e71b5de9360396fe657da4d888914515
syedyshiraz <syedyakoobshiraz@github.com>
2018-07-29 16:45:48 +0530

    solution to SPOJ problem

---
+++ half of the half.py
@@ -0,0 +1,8 @@
+t=int(input('enter the test cases: '))
+for i in range(t):
+    s=str(input('\nenter the string: '))
+    for j in range(0,len(s)//2,2):
+        print(s[j],end='')
+            
+
+        
